dfd384748c4d37ab0afd41b976f5fc4517e16366
Andrew Tran <andrew.tran@tapinfluence.com>
2015-01-07 15:23:54 -0700

    Initial Code Commit from Rubymine

--- server.js
+++ server.js
@@ -23,10 +23,10 @@
  */
 // set up ========================
 var express = require('express');
-var app = express();                                                               // create our app w/ express
-var mongoose = require('mongoose');                                     // mongoose for mongodb
-var morgan = require('morgan');                         // log requests to the console (express4)
-var bodyParser = require('body-parser');        // pull information from HTML POST (express4)
+var app = express(); // create our app w/ express
+var mongoose = require('mongoose'); // mongoose for mongodb
+var morgan = require('morgan'); // log requests to the console (express4)
+var bodyParser = require('body-parser') // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var argv = require('optimist').argv;
 
@@ -34,77 +34,57 @@ var argv = require('optimist').argv;
 mongoose.connect('mongodb://' + argv.be_ip + ':80/my_database');
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.use(morgan('dev'));                                                                                 // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'}));                    // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                                                     // parse application/json
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride());
+
 // define model =================
-var Todo = mongoose.model('Todo', {
-  title: String,
-  completed: Boolean
+var Message = mongoose.model('Message', {
+  sender: String,
+  recipient: String,
+  time: String,
+  body: String
 });
+
 // routes ======================================================================
 
 // api ---------------------------------------------------------------------
 
-// get all todos
-app.get('/api/todos', function (req, res) {
-  // use mongoose to get all todos in the database
-  Todo.find(function (err, todos) {
-    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-    if (err)
-      res.send(err)
-    res.json(todos); // return all todos in JSON format
-  });
-});
-
-// create todo and send back all todos after creation
-app.post('/api/todos', function (req, res) {
-  // create a todo, information comes from AJAX request from Angular
-  Todo.create({
-    title: req.body.title,
-    completed: false
-  }, function (err, todo) {
-    if (err)
-      res.send(err);
-// get and return all the todos after you create another
-    Todo.find(function (err, todos) {
-      if (err)
-        res.send(err)
-      res.json(todos);
-    });
-  });
-});
-
-app.put('/api/todos/:todo_id', function (req, res) {
-  return Todo.findById(req.params.todo_id, function (err, todo) {
-    todo.title = req.body.title;
-    todo.completed = req.body.completed;
-    return todo.save(function (err) {
-      if (err) {
-        res.send(err);
+//Get Messages
+app.get('/api/messages', function (req, res) {
+  var me = "S_K";
+  Message
+    .where('recipient').in(['all', me])
+    .exec(
+      function(err, messages){
+        if (err) {
+          res.send(err);
+        }
+        res.json(messages);
       }
-      return res.send(todo);
-    });
+    );
+});
+
+//Create Message
+app.post('/api/messages', function (req, res) {
+  var now = Date.now();
+  Todo.create({
+    sender: 'S_K',
+    recipient: 'all',
+    time: now,
+    body: 'lolwut'
+  }, function (err, todo) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(todo);
   });
 });
 
-// delete a todo
-app.delete('/api/todos/:todo_id', function (req, res) {
-  Todo.remove({
-    _id: req.params.todo_id
-  }, function (err, todo) {
-    if (err)
-      res.send(err);
-    // get and return all the todos after you create another
-    Todo.find(function (err, todos) {
-      if (err)
-        res.send(err)
-      res.json(todos);
-    });
-  });
-});
+//Don't need any puts or deletes for chat.
+
 // application -------------------------------------------------------------
 app.get('/', function (req, res) {
   res.sendfile('index.html'); // load the single view file (angular will handle the page changes on the front-end)
